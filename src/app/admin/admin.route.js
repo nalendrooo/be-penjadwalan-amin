2b4f21c612da81aa97a07b28b80cce1f7babba04
Nalendrooo <nalendroadil274@gmail.com>
2024-11-17 17:13:27 +0700

    delete comment

--- src/app/admin/admin.route.js
+++ src/app/admin/admin.route.js
@@ -7,13 +7,11 @@ import { validateRequest } from "../../middleware/validate-request.js";
 
 const route = Router()
 
-// route.get('/', mataPelajaranService.getMataPelajaran);
 route.get('/guru', adminService.getAllGuru);
 route.get('/siswa', adminService.getAllSiswa);
 route.get('/kelas', adminService.getAllKelas);
 route.patch('/kelas/:id/guru', validateRequest(adminSchema.addGuruToKelasSchema), adminService.addGuruToKelas);
 route.post('/kelas', validateRequest(adminSchema.createKelasSchema), adminService.createClass);
 route.post('/guru/register/:role_id', validateRequest(usersSchema.createUserSchema), usersService.createUser);
-// route.patch('/:id', validateRequest(mataPelajaranSchema.mataPelajaranSchema), mataPelajaranService.updateMataPelajaran);
 
 export default route    
