468839a0be29143745ed808ea93ed9e42ac5baa5
Nalendrooo <nalendroadil274@gmail.com>
2025-06-05 09:19:49 +0700

    d

--- src/app/users/users.request.js
+++ src/app/users/users.request.js
@@ -5,6 +5,7 @@ import { joiGeneralMessage } from '../../utils/joi.js'
 export const createUserSchema = Joi.object({
     email: Joi.string().email().required().messages(joiGeneralMessage),
     password: Joi.string().required().min(8).max(12).messages(joiGeneralMessage),
+    wali: Joi.string().optional().messages(joiGeneralMessage),
     nis: Joi.string().messages(joiGeneralMessage),
     nama: Joi.string().required().messages(joiGeneralMessage),
     telephone: Joi.string().required().messages(joiGeneralMessage),
