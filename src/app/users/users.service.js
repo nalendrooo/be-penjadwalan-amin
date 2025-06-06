2b7aa7c9d3eb0ec3b503a8344746aa8cfb85e3c3
Nalendrooo <nalendroadil274@gmail.com>
2025-06-06 14:32:49 +0700

    p

--- src/app/users/users.service.js
+++ src/app/users/users.service.js
@@ -110,9 +110,8 @@ export const updatePasswordByEmail = async (req, res) => {
 
     const salt = await bcrypt.genSalt()
     const hashPassword = await bcrypt.hash(body.password, salt)
-console.log(body)
     const data = await userRepository.updatePasswordByEmail(body.email, hashPassword)
-
+console.log(data)
     return res.status(200).json({
         status: 'success',
         message: 'User berhasil direset',
