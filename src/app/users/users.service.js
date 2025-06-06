5af78cfcc31a21a76c722070b45d1f55ad86e639
Nalendrooo <nalendroadil274@gmail.com>
2025-06-06 14:20:07 +0700

    p

--- src/app/users/users.service.js
+++ src/app/users/users.service.js
@@ -111,7 +111,7 @@ export const updatePasswordByEmail = async (req, res) => {
     const salt = await bcrypt.genSalt()
     const hashPassword = await bcrypt.hash(body.password, salt)
 
-    const data = await userRepository.updatePasswordByEmail(user.id, hashPassword)
+    const data = await userRepository.updatePasswordByEmail(body.email, hashPassword)
 
     return res.status(200).json({
         status: 'success',
